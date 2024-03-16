1a9a59f1b797b2bc074c57e34e430b30cae66deb
jcampolim <julia.campolim@gmail.com>
2024-03-16 18:02:55 -0300

    readFile funcionando + alocação dinamica

--- trabalho-1/main.cpp
+++ trabalho-1/main.cpp
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
 
 #define MAXN 50
 
-void readFile(char maze[][MAXN], int *n, int *m) {
+void getFileSize(int *n, int *m) {
     FILE* file = fopen("entrada.txt", "r");
 
     if(file == NULL) {
@@ -14,95 +17,97 @@ void readFile(char maze[][MAXN], int *n, int *m) {
 
     *n = a;
     *m = b;
-    
-    for(int i = 0; i <= *n; i++) {
-        for(int j = 0; j <= *m; j++) {
-            maze[i][j] = fgetc(file);
+
+    fclose(file);
+}
+
+void getFileContent(char** maze, int n, int m) {
+    FILE* file = fopen("entrada.txt", "r");
+
+    if(file == NULL) {
+        printf("Erro ao abrir o arquivo!\n");
+    }
+
+    char linha[m];
+
+    fscanf(file, "%s", linha);
+    fscanf(file, "%s", linha);
+
+    for(int i = 0; i < n; i++) {
+        fscanf(file, "%s", linha);
+        for (int j = 0; j < m; ++j) {
+            maze[i][j] = linha[j];
         }
     }
 
     fclose(file);
 }
 
-void printMaze(char maze[][MAXN], int n, int m) {
-    for(int i = 0; i <= n; i++) {
-        for(int j = 0; j <= m; j++) {
+void printMaze(char** maze, int n, int m) {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
             printf("%c", maze[i][j]);
         }
+        printf("\n");
     }
+    printf("\n");
 }
 
-void solveMaze(char maze[][MAXN], char solution[], int n, int m, int steps) {
-    maze[1][1] = 'E';
-    for(int i = 0; i <= n; i++) {
-        for(int j = 0; j <= m; j++) {
-            if(maze[i][j] == 'x') {
-                maze[i][j] = '.';
-            }
-        }
-    }
-
-    int i = 1, j = 2;
-    for(int k = 0; k < steps; k++) {
-        if(solution[k] == 'R') j++;
-        else if(solution[k] == 'L') j--;
-        else if(solution[k] == 'D') i++;
-        else if(solution[k] == 'U') i--;
-        else printf("a");
-        
-        maze[i][j] = 'o';
-    }
-}
-
-void findPath(char maze[][MAXN], char testingPath[], char solution[], int n, int m, int i, int j, int steps) {
+bool findPath(char** maze, int n, int m, int i, int j) {
     if(maze[i][j] == 'S') {
-        printf("%d\n", steps);
-        for(int k = 0; k <= steps; k++) {
-            solution[k] = testingPath[k];
-        }
-
-        return;
+        return true;
     } else {
-        if((i > 0 && j > 0) && (i < n && j < m)) {
-            maze[i][j] = 'x';
-            if(maze[i][j+1] != '#' && maze[i][j+1] != 'x') {
-                steps++;
-                testingPath[steps] = 'R';
-                findPath(maze, testingPath, solution, n, m, i, j+1, steps);
-            }
-            if(maze[i][j-1] != '#' && maze[i][j-1] != 'x') {
-                steps++;
-                testingPath[steps] = 'L';
-                findPath(maze, testingPath, solution, n, m, i, j-1, steps);
-            }
-            if(maze[i+1][j] != '#' && maze[i+1][j] != 'x') {
-                steps++;
-                testingPath[steps] = 'D';
-                findPath(maze, testingPath, solution, n, m, i+1, j, steps);
-            }
-            if(maze[i-1][j] != '#' && maze[i-1][j] != 'x') {
-                steps++;
-                testingPath[steps] = 'U';
-                findPath(maze, testingPath, solution, n, m, i-1, j, steps);
-            }
+        if((i >= 0 && j >= 0) && (i < n && j < m)) {
+            if (maze[i][j] == '.'){
+               maze[i][j] = 'o';
 
-            return;
+               if(maze[i][j+1] == '.' || maze[i][j+1] == 'S') {
+                   if (findPath(maze, n, m, i, j+1)) 
+                       return true;
+               }          
+
+               if(maze[i][j-1] == '.' || maze[i][j-1] == 'S') {
+                   if(findPath(maze, n, m, i, j-1))
+                       return true;
+               }
+
+               if(maze[i+1][j] == '.' || maze[i+1][j] == 'S') {
+                  if(findPath(maze, n, m, i+1, j))
+                     return true;
+               }
+
+               if(maze[i-1][j] == '.' || maze[i-1][j] == 'S') {
+                 if (findPath(maze, n, m, i-1, j))
+                    return true;
+               }
+
+               maze[i][j] = '.';
+           }
         }
     }
+    return false;
 }
 
 int main() {
-    int n, m;    
-    char maze[MAXN][MAXN];
+    int n, m;
+    getFileSize(&n, &m);
 
-    readFile(maze, &n, &m);
-    char testingPaths[n*m], solution[n*m];
+    char** maze = (char **)malloc(n * sizeof(char *));
+    for(int i = 0; i< n; i++) {
+        maze[i] = (char *)malloc(m * sizeof(char));
+    }
 
-    findPath(maze, testingPaths, solution, n, m, 1, 1, 0);
-
-    solveMaze(maze, solution, n, m, 50);
+    getFileContent(maze, n, m);
 
     printMaze(maze, n, m);
 
+    bool isPossible = findPath(maze, n, m, 1, 1);
+    if(isPossible) {
+        printMaze(maze, n, m);  
+    } else {
+        printf("Não foi possível encontrar a saida do labirinto :(\n");
+    }
+
+    free(maze);
     return 0;
 }
